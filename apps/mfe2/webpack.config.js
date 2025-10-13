f4b56584777ba8940b159c156968810f051cf00a
Muazzam Nashat <muazzamhc.nashat@gmail.com>
2025-10-13 17:32:11 -0400

    fix mfe2 webpack config

--- apps/mfe2/webpack.config.js
+++ apps/mfe2/webpack.config.js
@@ -1,22 +1,39 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-const config = withModuleFederationPlugin({
+const path = require('path');
+const mf = require('@angular-architects/module-federation/webpack');
+const { withModuleFederationPlugin, shareAll } = mf;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.base.json'), // path to Nx root tsconfig
+  ['@shared/ng-ui']
+);
+
+const base = withModuleFederationPlugin({
   name: 'mfe2',
-  filename: "remoteEntry.js",
-  exposes: {
-    './web-components': './src/bootstrap.ts',
-  },
+  filename: 'remoteEntry.js',
+  exposes: { './web-components': './src/bootstrap.ts' },
   shared: {
-    ...shareAll({ singleton: false, strictVersion: true, requiredVersion: 'auto' }),
-    '@shared/ng-ui': {
-      singleton: true,
-      strictVersion: false,
-      requiredVersion: 'auto',
-      import: 'shared/src/index',
-    }
-  }
+    // Angular & rxjs singletons:
+    ...shareAll({ singleton: false, strictVersion: true, requiredVersion: 'auto' }), //false because we are running different versions of angular
+    // Add descriptors for your mapped libs so share keys match import specifiers:
+    ...sharedMappings.getDescriptors(),
+  },
+  // IMPORTANT: do NOT also pass `sharedMappings: ['@shared/ng-ui']` here
 });
 
-console.log(config);
-
-module.exports = config;
+// Merge aliases and the SharedMappings plugin exactly once
+module.exports = {
+  ...base,
+  resolve: {
+    ...(base.resolve || {}),
+    alias: {
+      ...((base.resolve && base.resolve.alias) || {}),
+      ...sharedMappings.getAliases(),
+    },
+  },
+  plugins: [
+    ...(base.plugins || []),
+    sharedMappings.getPlugin(),
+  ],
+};
